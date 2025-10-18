2b21864339bcbffd6b41591b9b8741559d0f975c
shoojoy <157701033+shoojoy@users.noreply.github.com>
2025-10-19 00:29:34 +0900

    feat: 결과를 '결과 : <합>' 형식으로 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,7 +7,10 @@ public class Application {
         // TODO: 프로그램 구현
         // 시작 문구 출력하기
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-
         String input = Console.readLine();
+
+        //합하여 출력
+        int result = new StringCalculator().sum(input);
+        System.out.println("결과 : " + result);
     }
 }

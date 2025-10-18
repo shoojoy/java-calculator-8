e0f0892a5077fd935de83fbb3a5fccb25b9b471d
shoojoy <157701033+shoojoy@users.noreply.github.com>
2025-10-18 23:24:46 +0900

    feat: 시작 문구 출력 및 한 줄 입력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,13 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // 시작 문구 출력하기
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        String input = Console.readLine();
     }
 }
